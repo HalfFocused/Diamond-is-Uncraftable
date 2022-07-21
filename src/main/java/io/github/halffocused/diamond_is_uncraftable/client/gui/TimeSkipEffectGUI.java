326b2159c0d0b5673645d9aac69910dc8c71784b
HalfFocused <halffocused8@gmail.com>
2022-07-21 17:11:38 -0400

    Test commit

--- src/main/java/io/github/halffocused/diamond_is_uncraftable/client/gui/TimeSkipEffectGUI.java
+++ src/main/java/io/github/halffocused/diamond_is_uncraftable/client/gui/TimeSkipEffectGUI.java
@@ -50,6 +50,7 @@ public class TimeSkipEffectGUI extends AbstractGui {
                     //This was a very informative introduction to GUIs. What did I learn? Why I couldn't find a guide online.
                     double xScaleFactor = mc.getMainWindow().getScaledWidth() / 256.0;
                     double yScaleFactor = mc.getMainWindow().getScaledHeight() / 154.0;
+                    //Test
 
 
                     GlStateManager.enableBlend();
