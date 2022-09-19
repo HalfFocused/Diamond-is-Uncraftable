4b69e23761ff6ff63c4dcc7efc6019eb9a31382a
HalfFocused <halffocused8@gmail.com>
2022-09-18 21:50:58 -0400

    cleanup duty

--- src/main/java/io/github/halffocused/diamond_is_uncraftable/init/EffectInit.java
+++ src/main/java/io/github/halffocused/diamond_is_uncraftable/init/EffectInit.java
@@ -14,10 +14,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class EffectInit {
     public static DeferredRegister<Effect> EFFECTS = DeferredRegister.create(ForgeRegistries.POTIONS, DiamondIsUncraftable.MOD_ID);
 
-    public static final RegistryObject<Effect> OXYGEN_POISONING = EFFECTS.register("oxygen_poisoning",
-            () -> new OxygenPoisoningEffect(EffectType.HARMFUL, 7009526)
-                    .addAttributesModifier(Attributes.MOVEMENT_SPEED, "81d95dec-554d-4199-adbc-a339d2a671d3", -0.2f, AttributeModifier.Operation.MULTIPLY_TOTAL));
-
     public static final RegistryObject<Effect> HAZE = EFFECTS.register("haze",
             () -> new HazeEffect(EffectType.HARMFUL, 9250166));
 
@@ -26,13 +22,4 @@ public class EffectInit {
 
     public static final RegistryObject<Effect> STAND_STRENGTH = EFFECTS.register("stand_strength",
             () -> new StandStrengthEffect(EffectType.BENEFICIAL, 5826842));
-
-    public static final RegistryObject<Effect> AGING = EFFECTS.register("aging",
-            () -> new AgingEffect(EffectType.HARMFUL, 4606017)
-                    .addAttributesModifier(Attributes.MOVEMENT_SPEED, "a660e8a9-dcec-48e6-abad-558df89b75fe", -0.1f, AttributeModifier.Operation.ADDITION)
-                    .addAttributesModifier(Attributes.FOLLOW_RANGE, "a660e8a9-dcec-48e6-abad-558df89b75fe", -0.2f, AttributeModifier.Operation.ADDITION)
-                    .addAttributesModifier(Attributes.KNOCKBACK_RESISTANCE, "a660e8a9-dcec-48e6-abad-558df89b75fe", -0.2f, AttributeModifier.Operation.ADDITION)
-                    .addAttributesModifier(Attributes.MAX_HEALTH, "a660e8a9-dcec-48e6-abad-558df89b75fe", -0.2f, AttributeModifier.Operation.ADDITION)
-                    .addAttributesModifier(Attributes.ATTACK_KNOCKBACK, "a660e8a9-dcec-48e6-abad-558df89b75fe", -0.2f, AttributeModifier.Operation.ADDITION)
-                    .addAttributesModifier(Attributes.ATTACK_DAMAGE, "a660e8a9-dcec-48e6-abad-558df89b75fe", -0.2f, AttributeModifier.Operation.ADDITION));
 }
